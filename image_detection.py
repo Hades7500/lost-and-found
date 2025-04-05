38388eb79b23d6a6912c23fb1a0371b5ecf6cc1a
Hades7500 <hades7963@protonmail.com>
2025-04-06 01:27:27 +0530

    Made image detection more accurate, Cleaned up unnessary code

--- image_detection.py
+++ image_detection.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
+import sys
 
 def image_identification(lost_item):
     tags = []
-    model = YOLO("yolov8s.pt")
-
+    model = YOLO("yolov8m.pt")
     results = model(lost_item)
 
     for result in results:
@@ -14,10 +14,15 @@ def image_identification(lost_item):
             class_id = int(box.cls)
             label = names[class_id]
             confidence = float(box.conf)
-            print(f"Detected: {label} ({confidence:.2f})")
-            if (confidence):
+            # print(f"Detected: {label} ({confidence:.2f})")
+            if (confidence > 0.85):
                 tags += [label]
     
     tags = [tag for tag in tags if (tag != "person")]
     
     return tags[:2]
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    tags = image_identification(file)
+    print(tags)
